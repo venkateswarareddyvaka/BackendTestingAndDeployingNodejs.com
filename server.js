e8c413f72087ca901e6928047dbf33ad2c85df0f
venkateswarareddyvaka <venkateshvaka81@gmail.com>
2024-05-08 00:05:12 +0530

    importing cors from cors package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoutes')
+const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
